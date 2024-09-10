c55797b6675ede67f82c73a196b0cd7487ccdf7e
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-09-10 16:54:44 +0700

    Initial Commit

---
+++ EstoreCRUD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EstoreCRUD
+//
+//  Created by irfan wahendra on 10/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EstoreCRUD/EstoreCRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstoreCRUDApp.swift
+//  EstoreCRUD
+//
+//  Created by irfan wahendra on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct EstoreCRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
